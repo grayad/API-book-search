d486db97c1b8eaf7e6c45a6eea3e4e8b7fdd2db7
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-09-04 16:42:22 -0400

    authorize users using token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { User, Book } = require("../models");
 
 const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -18,8 +19,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
 
-      return user;
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -34,7 +36,8 @@ const resolvers = {
         throw new AuthenticationError("Incorrect credentials");
       }
 
-      return user;
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
